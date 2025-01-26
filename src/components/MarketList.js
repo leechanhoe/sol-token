49f5308eb9e76269b8de1cf83839e45b5e7bb26b
102sae <102chimee@gmail.com>
2025-01-26 19:21:58 +0900

    style : 디자인 수정

--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -5,19 +5,25 @@ function MarketList() {
   const markets = [
     { id: 1, name: "탄소배출권", price: 12000, change: "+2.56%" },
     { id: 2, name: "KAU25", price: 11400, change: "+2.56%" },
-    { id: 3, name: "S&P GSCI Global Voluntary Carbon Liquidity Weighted (USD)", price: 11400, change: "+2.56%" },
+    {
+      id: 3,
+      name: "S&P GSCI Global Voluntary Carbon Liquidity Weighted (USD)",
+      price: 11400,
+      change: "+2.56%",
+    },
   ];
 
   return (
     <section className="market-list">
-      <h1>탄소배출권 시장</h1>
-      <p>프로젝트를 통해 얻은 크레딧을 거래해보세요</p>
+      <p className="title">탄소배출권 시장</p>
+      <p className="subtitle">프로젝트를 통해 얻은 크레딧을 거래해보세요</p>
+
       <ul>
         {markets.map((market) => (
           <li key={market.id} className="market-item">
-            <span>{market.name}</span>
-            <span className="price">{market.price.toLocaleString()} 원</span>
-            <span className="change">{market.change}</span>
+            <span className="name">{market.name}</span>
+            <span className="price">{market.price.toLocaleString()}</span>
+            <p className="change">{market.change}</p>
           </li>
         ))}
       </ul>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,11 +5,28 @@ import Header from "./Header";
 
 function ProjectList() {
   const navigate = useNavigate();
-
   const projects = [
-    { id: 1, title: "중고거래를 통한 온실가스 감축 사업", status: "진행중", amount: 4000, date: "24.01.01 - 25.01.01" },
-    { id: 2, title: "중고거래를 통한 온실가스 감축 사업", status: "진행중", amount: 4000, date: "24.01.01 - 25.01.01" },
-    { id: 3, title: "중고거래를 통한 온실가스 감축 사업", status: "진행중", amount: 4000, date: "24.01.01 - 25.01.01" },
+    {
+      id: 1,
+      projectId: "PC3029172638912",
+      title: "중고거래를 통한 온실가스 감축 사업",
+      amount: 4000,
+      date: "24.01.01 - 25.01.01",
+    },
+    {
+      id: 2,
+      projectId: "PC3029172638912",
+      title: "중고거래를 통한 온실가스 감축 사업",
+      amount: 4000,
+      date: "24.01.01 - 25.01.01",
+    },
+    {
+      id: 3,
+      projectId: "PC3029172638912",
+      title: "중고거래를 통한 온실가스 감축 사업",
+      amount: 4000,
+      date: "24.01.01 - 25.01.01",
+    },
   ];
 
   const handleNavigate = (id) => {
@@ -17,27 +34,36 @@ function ProjectList() {
   };
 
   return (
-    <section className="project-list">
+    <div>
       <Header />
-      <h2>프로젝트 현황</h2>
-      <p>최근 올라온 프로젝트를 확인해 보세요</p>
-      {projects.map((project) => (
-        <div
-          key={project.id}
-          className="project-item"
-          onClick={() => handleNavigate(project.id)} // 클릭 시 해당 ID로 이동
-        >
-          <h3>{project.title}</h3>
-          <p>{project.date} - <span className="project-status">{project.status}</span></p>
-          <p>
-            <span>예상 감축량</span> <strong>{project.amount} tCO₂eq</strong>
-          </p>
-        </div>
-      ))}
-      <div className="more-button-container">
-        <button className="more-button">더보기</button>
-      </div>
-    </section>
+      <section className="project-list">
+        <p className="title">프로젝트 현황</p>
+        <p className="subtitle">최근 올라온 프로젝트를 확인해 보세요</p>
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="project-item"
+            onClick={handleNavigate}
+          >
+            {/* 첫 번째 행 */}
+            <div className="row" onClick={() => handleNavigate(project.id)}>
+              <div className="col">
+                <p className="project-id">{project.projectId}</p>
+                <p className="project-title">{project.title}</p>
+                <p className="project-date">{project.date}</p>
+              </div>
+              <div className="col">
+                <p className="col-title">예상 감축량</p>
+                <strong>
+                  {project.amount.toLocaleString()}
+                  <span className="highlight"> tCO₂eq</span>
+                </strong>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
 
